fe556ca7e4c667bb58683af3cf3ec6d0c2015fac
BerkSuleyman <berk67@hotmail.se>
2017-03-08 11:07:27 +0100

    Version 1

---
+++ guess_the_number.rb
@@ -0,0 +1,10 @@
+print "Welcome to guess the number" 
+print "Select a difficulty"
+print "To play this game pick a number between 1-10 and keep guessing until you get the right number player 2 has chossen"
+running == true
+while running
+input == gets.chomp
+if input == "Quit"
+  running = false
+end
+end
